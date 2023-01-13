91ffde8cf444d31364a714f4dd9ac9c6fc7818c4
khaled <khaledb.yahya@gmail.com>
2023-01-13 21:35:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import Login from "./routes/Home";
+import Signup from "./routes/Home";
+import Account from "./routes/Home";
+import axios from "axios";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCoins(res.data);
+      console.log(res.data);
+    });
+  }, [url]);
+
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home coins={coins} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CoinSearch from "../components/CoinSearch";
+
+const Home = ({ coins }) => {
+  return (
+    <div>
+      <CoinSearch coins={coins} />
+    </div>
+  );
+};
+
+export default Home;
